21826b57fe24a5737701693cc192e7ee3b7c8721
adam-grub <kb9agt@hotmail.com>
2015-08-25 19:01:04 -0500

    Create routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# config/routes.rb
+
+require File.join(File.dirname(__FILE__), '../', 'lib', 'router.rb')
+require File.join(File.dirname(__FILE__), '../', 'lib', 'brain_rack')
+
+BrainRackApplication = BrainRack.new
+
+BrainRackApplication.router.config do
+  get "/test", :to => "custom#index"
+  get /.*/, :to => "custom#show"
+end
