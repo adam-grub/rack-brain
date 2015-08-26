0dd49a291310f015b0a0de648c1b7a9b463b6784
adam-grub <kb9agt@hotmail.com>
2015-08-25 19:24:38 -0500

    Create base_controller.rb

---
+++ app/base_controller.rb
@@ -0,0 +1,11 @@
+# app/controllers/base_controller.rb
+
+require File.join(File.dirname(__FILE__), '../', '../', 'lib', 'response')
+
+class BaseController
+  attr_reader :env
+
+  def initialize env
+    @env = env
+  end
+end
